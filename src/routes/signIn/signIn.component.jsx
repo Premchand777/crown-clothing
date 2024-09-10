b08f819fec3f0cf35c825bbeb50d1015c73ec628
Premchand Kodali <premchand.kodali@iprotechs.com>
2024-09-11 01:28:50 +0530

    added google sign in call and creating user call in onclick buttion event handler

--- src/routes/signIn/signIn.component.jsx
+++ src/routes/signIn/signIn.component.jsx
@@ -1,8 +1,15 @@
+import { signInWithGooglePopup, createUserDoc } from "../../utils/firebase.utils";
 
 const SignIn = () => {
+  const logGoogleUser = async () => {
+    const result = await signInWithGooglePopup();
+    // console.log(result);
+    await createUserDoc(result.user);
+  }
   return (
     <div>
       <h2>SIGN IN PAGE</h2>
+      <button type="button" onClick={logGoogleUser}>Signin With Google</button>
     </div>
   );
 }
