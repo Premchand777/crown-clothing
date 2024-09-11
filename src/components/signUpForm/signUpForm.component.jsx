1b89c8caa45d494d0424a51629d7676d23eb28f4
Premchand777 <65839523+Premchand777@users.noreply.github.com>
2024-09-11 21:25:17 +0530

    implemented sign up form component with form input component with onChange and onSubmit handlers for creating firebase user upon signing in.

---
+++ src/components/signUpForm/signUpForm.component.jsx
@@ -0,0 +1,93 @@
+import { useState } from "react";
+import { createAuthUserWithEmailPassword, createUserDoc } from '../../utils/firebase.utils';
+import FormInput from "../formInput/formInput.component";
+
+const defaultFormFields = {
+  displayName: '',
+  email: '',
+  password: '',
+  confirmPassword: ''
+}
+const SignUpForm = () => {
+  const [formFields, setFormFields] = useState(defaultFormFields);
+
+  const onChangeHandler = (event) => {
+    const { name, value } = event.target;
+    setFormFields({ ...formFields, [name]: value });
+  }
+
+  const onSubmitHandler = async (event) => {
+    event.preventDefault();
+    if (formFields.password !== formFields.confirmPassword) {
+      alert("password does not match");
+      return;
+    }
+    try {
+      const result = await createAuthUserWithEmailPassword(formFields.email, formFields.password);
+      result.user.displayName = formFields.displayName;
+      await createUserDoc(result.user);
+      setFormFields(defaultFormFields);
+    } catch (error) {
+      if (error.code === 'auth/email-already-in-use') {
+        alert("Email already is in use");
+      }
+    }
+  }
+  
+  return (
+    <div>
+      <h2>Signup with Email & Password</h2>
+      <form onSubmit={onSubmitHandler}>
+        {/* <label htmlFor="dn">Display Name</label>
+        <input type="text" name="displayName" id='dn' value={formFields.displayName} onChange={onChangeHandler} autoComplete='true' required /> */}
+        <FormInput
+          htmlFor='dn'
+          label='Display Name'
+          type='text'
+          name='displayName'
+          id='dn'
+          value={formFields.displayName}
+          onChange={onChangeHandler}
+          autoComplete='true'
+          required
+        />
+        <FormInput
+          htmlFor='el'
+          label='Email'
+          type='email'
+          name='email'
+          id='el'
+          value={formFields.email}
+          onChange={onChangeHandler}
+          autoComplete='true'
+          required
+        />
+        <FormInput
+          htmlFor='pwd'
+          label='Password'
+          type='password'
+          name='password'
+          id='pwd'
+          value={formFields.password}
+          onChange={onChangeHandler}
+          autoComplete='true'
+          required
+        />
+        <FormInput
+          htmlFor='cpwd'
+          label='Confirm Password'
+          type='password'
+          name='confirmPassword'
+          id='cpwd'
+          value={formFields.confirmPassword}
+          onChange={onChangeHandler}
+          autoComplete='true'
+          required
+        />
+        <button type="submit">Signup</button>
+      </form>
+    </div>
+  );
+}
+
+export default SignUpForm;
