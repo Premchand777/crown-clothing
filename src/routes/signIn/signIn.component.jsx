b1b0d5443c8f86e7b1f54de31aeabc8b65d91629
Premchand777 <65839523+Premchand777@users.noreply.github.com>
2024-09-11 21:25:48 +0530

    integrated sign up form in signin route component.

--- src/routes/signIn/signIn.component.jsx
+++ src/routes/signIn/signIn.component.jsx
@@ -1,15 +1,16 @@
 import { signInWithGooglePopup, createUserDoc } from "../../utils/firebase.utils";
+import SignUpForm from '../../components/signUpForm/signUpForm.component';
 
 const SignIn = () => {
   const logGoogleUser = async () => {
     const result = await signInWithGooglePopup();
-    // console.log(result);
     await createUserDoc(result.user);
   }
   return (
     <div>
       <h2>SIGN IN PAGE</h2>
       <button type="button" onClick={logGoogleUser}>Signin With Google</button>
+      <SignUpForm />
     </div>
   );
 }
